bebc563ec91bd0aacd92b03007d8c5b15e264c72
AnnaVorobyeva531 <anya.vorobyeva.2015@gmail.com>
2021-01-17 00:12:38 +0500

    видео 8, задание:Определение числа Фибонначчи + Генератор списка рядом Фибонначчи

--- lab.work_8/8.py
+++ lab.work_8/8.py
@@ -8,4 +8,18 @@ for n in range(int(element - 2)):
     cur = tmp
 print(str(element) + ' элемент последовательности равен ' + str(cur))
 
+# Генератор списка рядом Фибонначчи
+print("Введите размер списка")
+dimension = int(input())
+
+
+def fibonacci(n):
+    a, b = 1, 1
+    for i in range(n):
+        yield a
+        a, b = b, a + b
+
+
+data = list(fibonacci(dimension))
+print(data)
 
